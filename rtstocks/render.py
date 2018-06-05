c3fdc85865cfaf0ac42c661a0dac0a99acebace1
othalla <othalla.lf@gmail.com>
2018-06-05 22:55:45 +0200

    [Fix] print_table function

--- rtstocks/render.py
+++ rtstocks/render.py
@@ -1,3 +1,10 @@
+from typing import List
 from tabulate import tabulate
 
 
+def print_table(headers: list, stocks_data: List[list],
+                table_builder=tabulate) -> None:
+    table = table_builder(stocks_data,
+                          headers=headers,
+                          tablefmt='grid')
+    print(table)
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,9 +1,16 @@
 from unittest.mock import Mock
-from rtstocks.render import table
+from rtstocks.render import print_table
 
 
 def test_show_table_from_data():
-    table_builder = Mock(name='tabulate')
-    table([['symbol', 'latestPrice', 'latestSource'],
-        ['AAPL', 158.73, 'Previous close']], table_builder=table_builder)
-    table_builder.assert_called_once()
+    table = Mock(name='tabulate')
+    print_table(['symbol', 'latestPrice', 'latestSource'],
+                [['AAPL', 158.73, 'Previous close'],
+                 ['AMZN', 1158.73, 'Previous close']],
+                table_builder=table)
+    table.assert_called_once_with(
+        [['AAPL', 158.73, 'Previous close'],
+         ['AMZN', 1158.73, 'Previous close']],
+        headers=['symbol', 'latestPrice', 'latestSource'],
+        tablefmt='grid')
+    table.assert_called_once()
