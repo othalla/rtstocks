462004167b528f66e823e8e9b9ea5ec6f3265686
othalla <othalla.lf@gmail.com>
2018-06-08 00:21:03 +0200

    [Add] main print tab from list of stocks

--- rtstocks/main.py
+++ rtstocks/main.py
@@ -4,4 +4,6 @@ from rtstocks.render import stocks_datas, print_table
 
 if __name__ == "__main__":
     stocks = sys.argv[1:]
+    print_table(['Symbol', 'LatestSource', 'LatestPrice'],
+                stocks_datas(stocks))
 
