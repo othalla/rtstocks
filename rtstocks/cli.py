75a1debcc07fdf95da619f9829c4afb1a5992bc1
othalla <othalla.lf@gmail.com>
2018-06-09 22:30:17 +0200

    [Enh] refacto to priv function & prepare curses refresh

--- rtstocks/cli.py
+++ rtstocks/cli.py
@@ -5,6 +5,13 @@ from rtstocks.render import stocks_datas, table
 def stocks():
     parser = argparse.ArgumentParser()
     parser.add_argument('--stocks', nargs='*', required=True)
-    cli_params = parser.parse_args()
-    print(table(['Symbol', 'LatestSource', 'LatestPrice'],
-                stocks_datas(cli_params.stocks)))
+    parser.add_argument('--refresh', action='store_true')
+    _cli_behavior(parser.parse_args())
+
+
+def _cli_behavior(namespace: argparse.Namespace):
+    if namespace.refresh:
+        raise NotImplementedError
+    else:
+        print(table(['Symbol', 'LatestSource', 'LatestPrice'],
+                    stocks_datas(namespace.stocks)))
